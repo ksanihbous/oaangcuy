b86acc4c7534cd92aea05bdf2006472663d3a009
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-17 10:03:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -163,22 +163,22 @@ def handle_message(event):
 		"""
 		this is example if u just want to send a text message
 		"""
-		sendMessage('Hello Kampank!')
-        r=requests.get("https://api.haipbis.xyz/randomtwitchquotes")
-        data=r.text
-        data=json.loads(data)
-        qts1 = "{}".format(data["quotes"])
-        strm = "{}".format(data["streamer"])
-        r2=requests.get("http://ariapi.herokuapp.com/api/trans?key=beta&to=in&text={}".format(qts1))
-        data=r2.text
-        data2=json.loads(data)
-        qts2 = "{}".format(data2["result"]["translated"])
-        ret_ = "╭─「 Quotes Twitch 」"
-        ret_ += "\n├ Quotes EN : {}".format(qts1)
-        ret_ += "\n├ Quotes ID : {}".format(qts2)
-        ret_ += "\n├ Streamer : {}".format(strm)
-        ret_ += "\n╰─「 This Asa 」"
+        r = requests.get('https://api.haipbis.xyz/randomtwitchquotes')
+        data = r.text
+        data = json.loads(data)
+        qts1 = '{}'.format(data['quotes'])
+        strm = '{}'.format(data['streamer'])
+        r2 = requests.get('http://ariapi.herokuapp.com/api/trans?key=beta&to=in&text={}'.format(qts1))
+        data= r2.text
+        data2 = json.loads(data)
+        qts2 = '{}'.format(data2['result']['translated'])
+        ret_ = '╭─「 Quotes Twitch 」'
+        ret_ += '\n├ Quotes EN : {}'.format(qts1)
+        ret_ += '\n├ Quotes ID : {}'.format(qts2)
+        ret_ += '\n├ Streamer : {}'.format(strm)
+        ret_ += '\n╰─「 This Asa 」'
         sendMessage(str(ret_))
+		sendMessage('Hello Kampank!')
 
 	if text == 'hi2':
 		"""
