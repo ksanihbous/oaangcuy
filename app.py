504746b9d0d25f6af0564ff7d25efebc035b5123
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-17 16:15:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -166,14 +166,17 @@ def handle_message(event):
 		sendMessage('Wayaaeee~')
 
 	if text == 'quotes':
-        r=requests.get("https://api.haipbis.xyz/randomtwitchquotes")
-        data=r.text
-        data=json.loads(data)
+		"""
+		this is example if u just want to send a text message
+		"""
+        r = requests.get("https://api.haipbis.xyz/randomtwitchquotes")
+        data = r.text
+        data = json.loads(data)
         qts1 = "{}".format(data["quotes"])
         strm = "{}".format(data["streamer"])
-        r2=requests.get("http://ariapi.herokuapp.com/api/trans?key=beta&to=in&text={}".format(qts1))
-        data=r2.text
-        data2=json.loads(data)
+        r2 = requests.get("http://ariapi.herokuapp.com/api/trans?key=beta&to=in&text={}".format(qts1))
+        data = r2.text
+        data2 = json.loads(data)
         qts2 = "{}".format(data2["result"]["translated"])
         ret_ = "╭─「 Quotes Twitch 」"
         ret_ += "\n├ Quotes EN : {}".format(qts1)
