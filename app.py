036cdd57c9d3c95b9ef23fa68973a5f1141e099d
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-18 10:57:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -318,15 +318,15 @@ def handle_message(event):
 		#sendFlex(alt='Click For Login', contents=message)
 		sendMessage("Click Link QR Before 2 Minutes:\n{}".format(qr))
 		time.sleep(3)
-		name = line_bot_api.get_profile(sender).display_name
-		sendMessage("Click For Login {}".format(name))
+		#name = line_bot_api.get_profile(sender).display_name
+		sendMessage("Click For Login ")
 		result = json.loads(requests.get(result["result"]["callback"]+"&auth="+key).text)
 		if result["status"] != 200:
 			raise Exception("Timeout!!!")
 		pin = ""+result["result"]["pin_code"]
 		print("Pincode : "+pin)
 		time.sleep(3)
-		sendMessage('Pincode : {}'.format(pin))
+		sendMessage('Pincode : '+str(pin))
 		result = json.loads(requests.get(result["result"]["callback"]+"&auth="+key+"&sysname=SB Premium").text)
 		if result["status"] != 200:
 			raise Exception("Timeout!!!")
